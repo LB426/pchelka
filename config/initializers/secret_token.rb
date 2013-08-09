5c7db92d12c0eddd91e8ee1f6ba955f9e03a4c55
LB426 <LB426@tih.kuban.ru>
2013-08-09 11:08:09 +0400

    создан проект bee2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bee2::Application.config.secret_key_base = 'b148790d1e086d3dcd5c5aeb10aeab9e49968d7e873f3355e18d670dfb856d626d28045cbf1638d6c14c4f7b85485164c2cab8886e89cee290db134bdb72caef'
