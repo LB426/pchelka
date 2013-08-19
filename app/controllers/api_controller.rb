1ddda0c67c063e301800e9bfebe5f139e720e790
LB426 <LB426@tih.kuban.ru>
2013-08-19 17:45:27 +0400

    делаю дебильный api1

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,18 +4,24 @@ class ApiController < ApplicationController
   def queue
   	user = User.authenticate(params[:login], params[:password])
   	if user
-  		data = {
-  							:err => 'no',
-  							:res => [
-				  								{:did=>1, :qn=>3, :ln=>"дом быта"},
-				              		{:did=>2, :qn=>31, :ln=>"фонтан"}
-				              	]
-    				 }
-    	render :json => data, :only => [ :err, :res, :did, :qn, :ln ]
-    else
-    	data = { :err => 'autherror' }
-    	render :json => data, :only => [ :err ]
+  		#data = {
+  		#					:err => 'no',
+  		#					:res => [
+			#	  								{:did=>1, :qn=>3, :ln=>"дом быта"},
+			#	              		{:did=>2, :qn=>31, :ln=>"фонтан"}
+			#	              	]
+    	#			 }
+    	#render :json => data, :only => [ :err, :res, :did, :qn, :ln ]
+    #else
+    	#data = { :err => 'autherror' }
+    	#render :json => data, :only => [ :err ]
+    	@data = [
+    						{'name' => 'магнолия', 'queue' => [1,3,5] },
+    						{'name' => 'рынок', 'queue' => [2,4,6] },
+    						{'name' => 'та сторона', 'queue' => [8,9,10] }
+    					]
     end
+    render :layout => false
   end
   
   def test
