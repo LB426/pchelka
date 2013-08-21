49bb5b1ab524a476c463d82b1f0cf61347e9cd07
LB426 <LB426@tih.kuban.ru>
2013-08-21 08:33:27 +0400

    делаю работу с удалённой БД

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -20,6 +20,8 @@ class ApiController < ApplicationController
     						{'name' => 'рынок', 'queue' => [2,4,6] },
     						{'name' => 'та сторона', 'queue' => [8,9,10] }
     					]
+    else
+    	
     end
     render :layout => false
   end
