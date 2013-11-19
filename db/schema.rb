cf2c6bb3d3537603d53cde804797c2e062a75211
LB426 <LB426@tih.kuban.ru>
2013-11-19 18:20:45 +0400

    commit on pchelka srv

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115111653) do
+ActiveRecord::Schema.define(version: 20131113123919) do
 
   create_table "abonenty", id: false, force: true do |t|
     t.integer "num"
