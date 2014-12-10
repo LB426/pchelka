adcdf8b3a214ecee60da71c3a4496b205bbf2723
Andrey Saperov <developerc@yandex.ru>
2014-12-10 15:36:46 +0300

    делаю работу с координатами

---
+++ db/migrate/20141210122103_create_tracks.rb
@@ -0,0 +1,11 @@
+class CreateTracks < ActiveRecord::Migration
+  def change
+    create_table :tracks do |t|
+      t.integer :user_id
+      t.decimal :lon, precision: 13, scale: 10
+      t.decimal :lat, precision: 13, scale: 10
+
+      t.timestamps
+    end
+  end
+end
